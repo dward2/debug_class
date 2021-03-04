5e97ca608e3e2d60589bc62210947c63d25567a2
David Ward <david.a.ward@duke.edu>
2021-03-04 16:53:01 -0500

    Fix my sine function

--- debug_ex3.py
+++ debug_ex3.py
@@ -38,7 +38,7 @@ def sin_poly(x):
 
 def cos_poly(x):
     x_rad = x * math.pi / 180
-    cos = 1 - (x_rad**2) / 2 + (x_rad**4) / 24 - (x**6) / 720
+    cos = 1 - (x_rad**2) / 2 + (x_rad**4) / 24 - (x_rad**6) / 720
     return cos
 
 
